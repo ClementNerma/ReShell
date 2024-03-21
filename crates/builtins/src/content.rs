3b414a2acb6cb67b9a6ad8175100b8fd2a6d37c5
Clément Nerma <clement.nerma@gmail.com>
2024-03-21 15:36:00 +0100

    <lost commit message>

--- crates/builtins/src/content.rs
+++ crates/builtins/src/content.rs
@@ -312,6 +312,25 @@ pub fn define_native_lib() -> NativeLibDefinition {
             ),
             define_internal_fn!(
                 //
+                // Set an environment variable's value
+                //
+
+                "set_env",
+
+                Args [ArgsAt] (
+                    var_name: RequiredArg<StringType> = Arg::positional("var_name"),
+                    value: RequiredArg<StringType> = Arg::positional("value")
+                )
+
+                -> None,
+
+                |_, Args { var_name, value }, _, _| {
+                    std::env::set_var(var_name, value);
+                    Ok(None)
+                }
+            ),
+            define_internal_fn!(
+                //
                 // Change the current directory
                 //
 
