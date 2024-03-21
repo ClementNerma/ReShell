1c47010694f180d9b908ed066f3293f5c3d41809
Clément Nerma <clement.nerma@gmail.com>
2024-03-21 15:36:00 +0100

    <lost commit message>

--- crates/parser/src/program.rs
+++ crates/parser/src/program.rs
@@ -630,7 +630,6 @@ pub fn program() -> impl Parser<Program> {
                 && c != '\''
                 && c != '"'
                 && c != '$'
-                && c != '\\'
         })
         .repeated()
         .at_least(1)
