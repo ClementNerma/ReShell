d52ab8bce16cc4283a0b1e2bec1011f7de36742c
Clément Nerma <clement.nerma@gmail.com>
2024-03-21 15:36:00 +0100

    <lost commit message>

--- crates/builtins/src/functions/whereis.rs
+++ crates/builtins/src/functions/whereis.rs
@@ -2,7 +2,8 @@ crate::define_internal_fn!(
     "whereis",
 
     (
-        command: RequiredArg<StringType> = Arg::positional("command")
+        command: RequiredArg<StringType> = Arg::positional("command"),
+        lossy: PresenceFlag = Arg::long_flag("lossy")
     )
 
     -> Some(StringType::direct_underlying_type())
@@ -11,9 +12,10 @@ crate::define_internal_fn!(
 fn run() -> Runner {
     Runner::new(
         |_,
-         Args { command },
+         Args { command, lossy },
          ArgsAt {
              command: command_at,
+             ..
          },
          ctx| {
             let path = ctx
@@ -21,17 +23,23 @@ fn run() -> Runner {
                 .resolve_binary_path(&command)
                 .map_err(|err| ctx.error(command_at, format!("command not found: {err}")))?;
 
-            let path = path.to_str().ok_or_else(|| {
-                ctx.error(
-                    command_at,
-                    format!(
-                        "command path contains invalid UTF-8 characters: {}",
-                        path.display()
-                    ),
-                )
-            })?;
+            let path = if lossy {
+                path.to_string_lossy().into_owned()
+            } else {
+                path.to_str()
+                    .ok_or_else(|| {
+                        ctx.error(
+                            command_at,
+                            format!(
+                                "command path contains invalid UTF-8 characters: {}",
+                                path.display()
+                            ),
+                        )
+                    })?
+                    .to_owned()
+            };
 
-            Ok(Some(RuntimeValue::String(path.to_owned())))
+            Ok(Some(RuntimeValue::String(path)))
         },
     )
 }
