5c523deec66515932fb22d5027875c09a68205b4
clement-estone <clement.eraud@lonestone.studio>
2024-03-21 15:36:00 +0100

    <lost commit message>

---
+++ crates/builtins/src/methods/datetime/to_string.rs
@@ -0,0 +1,42 @@
+use time::format_description;
+use time::format_description::well_known::Rfc2822;
+
+use crate::functions::DateTimeValue;
+
+crate::define_internal_fn!(
+    "toString",
+
+    (
+        moment: RequiredArg<CustomType<DateTimeValue>> = Arg::method_self(),
+        format: OptionalArg<StringType> = Arg::positional("format")
+    )
+
+    -> Some(StringType::direct_underlying_type())
+);
+
+fn run() -> Runner {
+    Runner::new(|at, Args { moment, format }, args_at, ctx| {
+        let out = match format {
+            Some(format) => {
+                let format = format_description::parse(&format).map_err(|err| {
+                    ctx.throw(
+                        args_at.format.unwrap(),
+                        format!("Failed to parse date/time formatting: {err}"),
+                    )
+                })?;
+
+                moment
+                    .inner
+                    .format(&format)
+                    .map_err(|err| ctx.throw(at, format!("Failed to format date/time: {err}")))?
+            }
+
+            None => moment
+                .inner
+                .format(&Rfc2822)
+                .map_err(|err| ctx.throw(at, format!("Failed to format date/time: {err}")))?,
+        };
+
+        Ok(Some(RuntimeValue::String(out)))
+    })
+}
