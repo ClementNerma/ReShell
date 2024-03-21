f6c06045625f22c13788afb18c64cf092adb95cd
Clément Nerma <clement.nerma@gmail.com>
2024-03-21 15:36:00 +0100

    <lost commit message>

--- crates/runtime/src/conf.rs
+++ crates/runtime/src/conf.rs
@@ -4,13 +4,19 @@ use std::path::PathBuf;
 pub struct RuntimeConf {
     pub initial_home_dir: Option<PathBuf>,
     pub call_stack_limit: usize,
+    pub history_path: Option<PathBuf>,
+    pub history_enabled: bool,
+    pub history_capacity: usize,
 }
 
 impl Default for RuntimeConf {
     fn default() -> Self {
         Self {
             initial_home_dir: None,
-            call_stack_limit: 1000,
+            call_stack_limit: 10_000,
+            history_path: None,
+            history_enabled: true,
+            history_capacity: 1_000_000,
         }
     }
 }
