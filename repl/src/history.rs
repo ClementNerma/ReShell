477359aa198c824266409a4dc45c0d889a14b3ce
Clément Nerma <clement.nerma@gmail.com>
2024-03-21 15:36:00 +0100

    <lost commit message>

--- repl/src/history.rs
+++ repl/src/history.rs
@@ -1,5 +1,31 @@
 use reedline::{FileBackedHistory, History};
 
+use crate::print_warn;
+
 pub fn create_history() -> Box<dyn History> {
-    Box::new(FileBackedHistory::new(1000))
+    // TODO: allow to customize:
+    // * Enabling/disabling the history
+    // * Path to the history file
+    // * Obfuscation the history file (?)
+    // * History capacity
+
+    let capacity = 1_000_000;
+
+    let history = match dirs::home_dir() {
+        Some(dir) => match FileBackedHistory::with_file(capacity, dir.join(".rsh_history")) {
+            Ok(history) => history,
+            Err(err) => {
+                print_warn(&format!("Failed to use history file: {err}"));
+                print_warn("History will not be saved for this session");
+                FileBackedHistory::new(capacity)
+            }
+        },
+        None => {
+            print_warn("Failed to determine path to the home directory");
+            print_warn("History will not be saved for this session");
+            FileBackedHistory::new(capacity)
+        }
+    };
+
+    Box::new(history)
 }
