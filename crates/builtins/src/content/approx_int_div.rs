11b583885d926fba70b5e54fb844ffb30a0d9a75
Clément Nerma <clement.nerma@gmail.com>
2024-03-21 15:36:00 +0100

    <lost commit message>

--- crates/builtins/src/content/approx_int_div.rs
+++ crates/builtins/src/content/approx_int_div.rs
@@ -31,34 +31,14 @@ fn run() -> Runner {
     )
 }
 
-pub fn approx_int_div(a: u64, b: u64, mut precision: u8) -> String {
+pub fn approx_int_div(a: u64, b: u64, precision: u8) -> String {
     let max_prec = 10_u128.pow(u32::from(precision));
 
-    let div = u128::from(a) * max_prec / u128::from(b);
+    let div = u128::from(a) * max_prec * 10 / u128::from(b);
+    let div = (div / 10) + if div % 10 >= 5 { 1 } else { 0 };
 
-    let mut int_part = div / max_prec;
-    let mut frac_part = div % max_prec;
-
-    let last_digit = (u128::from(a) * max_prec * 10 / u128::from(b)) % 10;
-
-    if last_digit > 5 {
-        if precision == 0 {
-            int_part += 1;
-            frac_part = 0;
-        } else {
-            frac_part += 1;
-
-            if frac_part >= max_prec {
-                int_part += 1;
-                frac_part = 0;
-            }
-        }
-    }
-
-    while frac_part % 10 == 0 {
-        frac_part /= 10;
-        precision -= 1;
-    }
+    let int_part = div / max_prec;
+    let frac_part = div % max_prec;
 
     format!(
         "{int_part}{}",
