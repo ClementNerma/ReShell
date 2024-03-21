61d090e1daf8462b8293f050e992fb4d5a961c67
Clément Nerma <clement.nerma@gmail.com>
2024-03-21 15:36:00 +0100

    <lost commit message>

--- crates/builtins/src/content.rs
+++ crates/builtins/src/content.rs
@@ -294,7 +294,9 @@ pub fn define_native_lib() -> NativeLibDefinition {
                 -> None,
 
                 |at, Args { path }, ArgsAt { path: path_at }, _, ctx| {
-                    let path = Path::new(&path);
+                    let trimmed_path = path.trim_end_matches(['/', '\\']);
+
+                    let path = Path::new(if trimmed_path.is_empty() { path.as_str() } else { trimmed_path });
 
                     if !path.is_dir() {
                         return Err(ctx.error(path_at, format!("directory '{}' does not exist", path.display())))
