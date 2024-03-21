a57a9cb22f992ffd1a9b9255780f6f3079fe1e49
Clément Nerma <clement.nerma@gmail.com>
2024-03-21 15:36:00 +0100

    <lost commit message>

--- crates/repl/src/reports.rs
+++ crates/repl/src/reports.rs
@@ -1,6 +1,9 @@
 use std::borrow::Cow;
 
-use ariadne::{Fmt, Label, Report, ReportKind, Source};
+use annotate_snippets::{
+    display_list::{DisplayList, FormatOptions},
+    snippet::{Annotation, AnnotationType, Slice, Snippet, SourceAnnotation},
+};
 use colored::Colorize;
 use parsy::{CodeRange, Eaten, FileId, ParserExpectation, ParsingError, SourceFileID};
 use reshell_checker::CheckerError;
@@ -40,15 +43,15 @@ impl ReportableError {
 }
 
 pub fn print_error(err: &ReportableError, files: &FilesMap) {
-    let (at, msg) = match err {
+    let (at, nature, msg) = match err {
         ReportableError::Parsing(err) => {
             let (at, err) = parsing_error(err);
-            (RuntimeCodeRange::Parsed(at), err)
+            (RuntimeCodeRange::Parsed(at), "Syntax error", err)
         }
 
         ReportableError::Checking(err) => {
             let (at, err) = checking_error(err);
-            (RuntimeCodeRange::Parsed(at), err)
+            (RuntimeCodeRange::Parsed(at), "Checking error", err)
         }
 
         ReportableError::Runtime(err, _) => match &err.nature {
@@ -56,23 +59,33 @@ pub fn print_error(err: &ReportableError, files: &FilesMap) {
                 // This error is designed to be consumed, not displayed
                 unreachable!()
             }
+
             ExecErrorNature::Custom(message) => (
                 err.at,
+                "Runtime error",
                 match message {
                     Cow::Borrowed(str) => (*str).to_owned(),
                     Cow::Owned(string) => string.clone(),
                 },
             ),
+
             ExecErrorNature::ParsingErr(err) => {
                 let (at, err) = parsing_error(err);
-                (RuntimeCodeRange::Parsed(at), err)
+                (
+                    RuntimeCodeRange::Parsed(at),
+                    "Syntax error (at runtime)",
+                    err,
+                )
             }
+
             ExecErrorNature::CommandFailed {
                 message,
                 exit_status: _,
-            } => (err.at, message.clone()),
+            } => (err.at, "Command failed", message.clone()),
+
             ExecErrorNature::Thrown { value } => (
                 err.at,
+                "Function thrown",
                 format!(
                     "function throwned a value at {}",
                     dbg_loc(value.from, files).bright_magenta()
@@ -141,53 +154,56 @@ pub fn print_error(err: &ReportableError, files: &FilesMap) {
         SourceFileLocation::RealFile(path) => path.to_string_lossy().to_string(),
     };
 
-    let mut bottom = String::new();
+    let nature = format!("{}", nature.bright_red());
 
-    // if let ReportableError::Runtime(err, _) = err {
-    //     if let RuntimeCodeRange::Parsed(range) = err.scope_range {
-    //         let curr_scope_msg = format!("* In scope : {}", dbg_loc(range, files).bright_magenta());
-    //         bottom = format!("{}", curr_scope_msg.bright_yellow());
-    //     }
-    // }
+    let snippet = Snippet {
+        title: Some(Annotation {
+            label: Some(&nature),
+            id: None,
+            annotation_type: AnnotationType::Error,
+        }),
+        footer: vec![],
+        slices: vec![Slice {
+            source: &source_file.content,
+            line_start: 1, // TODO
+            origin: Some(&display_file),
+            fold: true, // ?
+            annotations: vec![SourceAnnotation {
+                label: &msg,
+                annotation_type: AnnotationType::Error,
+                range: (offset, offset + len),
+            }],
+        }],
+        opt: FormatOptions {
+            color: true,
+            ..Default::default()
+        },
+    };
+
+    eprintln!("{}", DisplayList::from(snippet));
+
+    let note = match err {
+        ReportableError::Parsing(_) => None,
+        ReportableError::Checking(_) => {
+            Some("Error was encountered before running the program".to_owned())
+        }
+        ReportableError::Runtime(err, _) => err.note.clone(),
+    };
+
+    if let Some(note) = note {
+        eprintln!("  = {} {note}", "note:".cyan());
+    }
 
     if let Some(call_stack) = call_stack {
         for entry in call_stack.history().iter().rev() {
             let CallStackEntry { fn_called_at } = entry;
 
-            let entry_msg = format!(
-                "* Called at: {}",
+            eprintln!(
+                "  = {} function called at: {}",
+                "note:".cyan(),
                 dbg_loc(*fn_called_at, files).bright_magenta()
             );
-
-            bottom.push_str(&format!("\n{}", entry_msg.fg(ariadne::Color::Yellow)));
         }
-    }
-
-    let mut inner = Report::build(ReportKind::Error, display_file.clone(), offset).with_label(
-        Label::new((display_file.clone(), offset..(offset + len)))
-            .with_message(msg.fg(ariadne::Color::Red).to_string())
-            .with_color(ariadne::Color::Red),
-    );
-
-    match err {
-        ReportableError::Parsing(_) => {}
-        ReportableError::Checking(_) => {
-            inner.set_note("Error was encountered before running the program")
-        }
-        ReportableError::Runtime(err, _) => {
-            if let Some(note) = &err.note {
-                inner.set_note(note);
-            }
-        }
-    };
-
-    inner
-        .finish()
-        .print((display_file, Source::from(source_file.content)))
-        .unwrap();
-
-    if !bottom.is_empty() {
-        println!("{bottom}");
     }
 }
 
