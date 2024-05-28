127291eb06b7e57b723f2254598a7eff01e9a627
Clément Nerma <clement.nerma@gmail.com>
2024-05-28 16:47:50 +0200

    Replace 'LazyCell' polyfill by nightly 'LazyLock' std implementation

--- crates/repl/src/paths.rs
+++ crates/repl/src/paths.rs
@@ -1,19 +1,17 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, sync::LazyLock};
 
-use crate::utils::lazy_cell::LazyCell;
+pub static HOME_DIR: LazyLock<Option<PathBuf>> = LazyLock::new(dirs::home_dir);
 
-pub static HOME_DIR: LazyCell<Option<PathBuf>> = LazyCell::new(dirs::home_dir);
+pub static SHELL_CONFIG_DIR: LazyLock<Option<PathBuf>> =
+    LazyLock::new(|| dirs::config_dir().map(|dir| dir.join("reshell")));
 
-pub static SHELL_CONFIG_DIR: LazyCell<Option<PathBuf>> =
-    LazyCell::new(|| dirs::config_dir().map(|dir| dir.join("reshell")));
+pub static SHELL_LOCAL_DATA_DIR: LazyLock<Option<PathBuf>> =
+    LazyLock::new(|| dirs::data_local_dir().map(|dir| dir.join("reshell")));
 
-pub static SHELL_LOCAL_DATA_DIR: LazyCell<Option<PathBuf>> =
-    LazyCell::new(|| dirs::data_local_dir().map(|dir| dir.join("reshell")));
+pub static INIT_SCRIPT_PATH: LazyLock<Option<PathBuf>> =
+    LazyLock::new(|| SHELL_CONFIG_DIR.as_ref().map(|dir| dir.join("init.rsh")));
 
-pub static INIT_SCRIPT_PATH: LazyCell<Option<PathBuf>> =
-    LazyCell::new(|| SHELL_CONFIG_DIR.as_ref().map(|dir| dir.join("init.rsh")));
-
-pub static HISTORY_PATH: LazyCell<Option<PathBuf>> = LazyCell::new(|| {
+pub static HISTORY_PATH: LazyLock<Option<PathBuf>> = LazyLock::new(|| {
     SHELL_LOCAL_DATA_DIR
         .as_ref()
         .map(|dir| dir.join("history.txt"))
--- crates/repl/src/utils/mod.rs
+++ crates/repl/src/utils/mod.rs
@@ -2,7 +2,6 @@ pub mod cmd_pieces;
 pub mod covering;
 pub mod ctrl_c;
 pub mod jaro_winkler;
-pub mod lazy_cell;
 pub mod nesting;
 pub mod path_globifier;
 pub mod syntax;
