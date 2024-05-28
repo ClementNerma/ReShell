d7ceef7a0ba48b5d0be1deb4c41db41949323464
Clément Nerma <clement.nerma@gmail.com>
2024-05-28 16:47:46 +0200

    <lost commit message>

--- crates/builtins/src/functions/write_file.rs
+++ crates/builtins/src/functions/write_file.rs
@@ -1,4 +1,8 @@
-use std::{fs, path::Path};
+use std::{
+    fs::{self, OpenOptions},
+    io::Write,
+    path::Path,
+};
 
 use crate::errors::FallibleAtRuntime;
 
@@ -11,22 +15,51 @@ crate::define_internal_fn!(
 
     (
         path: RequiredArg<StringType> = Arg::positional("path"),
-        content: RequiredArg<StringType> = Arg::positional("content")
+        content: RequiredArg<StringType> = Arg::positional("content"),
+        append: PresenceFlag = Arg::long_and_short_flag("append", 'a')
     )
 
     -> None
 );
 
 fn run() -> Runner {
-    Runner::new(|at, Args { path, content }, _, ctx| {
-        let path = Path::new(&path);
+    Runner::new(
+        |at,
+         Args {
+             path,
+             content,
+             append,
+         },
+         _,
+         ctx| {
+            let path = Path::new(&path);
 
-        fs::write(path, content).with_context(
-            || format!("failed to write file '{}'", path.display()),
-            at,
-            ctx,
-        )?;
+            if !append {
+                fs::write(path, content).with_context(
+                    || format!("failed to write file '{}'", path.display()),
+                    at,
+                    ctx,
+                )?;
+            } else {
+                OpenOptions::new()
+                    .create(true)
+                    .truncate(false)
+                    .append(true)
+                    .open(path)
+                    .with_context(
+                        || format!("failed to open file '{}'", path.display()),
+                        at,
+                        ctx,
+                    )?
+                    .write_all(content.as_bytes())
+                    .with_context(
+                        || format!("failed to append to file '{}'", path.display()),
+                        at,
+                        ctx,
+                    )?
+            }
 
-        Ok(None)
-    })
+            Ok(None)
+        },
+    )
 }
