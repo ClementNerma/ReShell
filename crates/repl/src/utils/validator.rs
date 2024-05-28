ea85fa99c9ad7c6b42004905fa9bd72e988c7552
Clément Nerma <clement.nerma@gmail.com>
2024-05-28 16:47:44 +0200

    <lost commit message>

--- crates/repl/src/utils/validator.rs
+++ crates/repl/src/utils/validator.rs
@@ -10,7 +10,9 @@ pub struct Validator;
 
 impl RlValidator for Validator {
     fn validate(&self, line: &str) -> ValidationResult {
-        if detect_nesting_actions(line).iter().any(|action| {
+        if line.trim_end().ends_with('\\') {
+            ValidationResult::Incomplete
+        } else if detect_nesting_actions(line).iter().any(|action| {
             matches!(
                 action.action_type,
                 NestingActionType::Opening {
