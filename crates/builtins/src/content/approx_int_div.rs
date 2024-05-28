5578a1807c9b99eb277b9c10af863084d18ca061
Clément Nerma <clement.nerma@gmail.com>
2024-05-28 16:47:37 +0200

    <lost commit message>

--- crates/builtins/src/content/approx_int_div.rs
+++ crates/builtins/src/content/approx_int_div.rs
@@ -40,16 +40,16 @@ pub fn approx_int_div(a: u64, b: u64, precision: u8) -> String {
     let int_part = div / max_prec;
     let frac_part = div % max_prec;
 
-    format!(
-        "{int_part}{}",
-        if frac_part > 0 && precision > 0 {
-            format!(
-                ".{:#0precision$}",
-                frac_part,
-                precision = usize::from(precision)
-            )
-        } else {
-            String::new()
-        }
-    )
+    let mut out = int_part.to_string();
+
+    if frac_part > 0 && precision > 0 {
+        out.push('.');
+        out.push_str(&format!(
+            "{:#0precision$}",
+            frac_part,
+            precision = precision.into()
+        ));
+    }
+
+    out
 }
