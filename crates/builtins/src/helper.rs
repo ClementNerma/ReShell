237256992f7fe74a42106ec6d206f80388bb5d26
Clément Nerma <clement.nerma@gmail.com>
2024-05-28 16:47:45 +0200

    <lost commit message>

--- crates/builtins/src/helper.rs
+++ crates/builtins/src/helper.rs
@@ -131,11 +131,6 @@ impl<const OPTIONAL: bool, T: Typing> Arg<OPTIONAL, T> {
             is_rest: false,
         }
     }
-
-    // pub fn rest(mut self) -> Self {
-    //     self.is_rest = true;
-    //     self
-    // }
 }
 
 impl<const OPTIONAL: bool, T: TypingDirectCreation> Arg<OPTIONAL, T> {
@@ -246,16 +241,20 @@ pub(super) fn generate_internal_arg_decl<
     arg: A,
 ) -> FnArg {
     match arg.names() {
-        ArgNames::Positional(name) => FnArg::Positional {
-            name: RuntimeEaten::Internal(
-                (*name).to_owned(),
-                "native library's arguments declaration",
-            ),
-            is_optional: arg.is_optional(),
-            typ: Some(RuntimeEaten::Internal(
-                arg.base_typing().underlying_type(),
-                "native library's arguments declaration",
-            )),
+        ArgNames::Positional(name) => if !arg.is_rest() {
+            FnArg::Positional {
+                name: RuntimeEaten::Internal(
+                    (*name).to_owned(),
+                    "native library's arguments declaration",
+                ),
+                is_optional: arg.is_optional(),
+                typ: Some(RuntimeEaten::Internal(
+                    arg.base_typing().underlying_type(),
+                    "native library's arguments declaration",
+                )),
+            }
+        } else {
+            FnArg::Rest { name: RuntimeEaten::Internal((*name).to_owned(), "native library's arguments declaration") }
         },
 
         ArgNames::Flag(flag) => {
