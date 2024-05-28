1592bcd9efc380745aad98f4f16a2164eea8e028
Clément Nerma <clement.nerma@gmail.com>
2024-05-28 16:47:32 +0200

    <lost commit message>

--- crates/builtins/src/content.rs
+++ crates/builtins/src/content.rs
@@ -107,6 +107,29 @@ pub fn define_native_lib() -> NativeLibDefinition {
                     }
                 ),
             ),
+            (
+                "dbg-type",
+                define_internal_fn!(
+                    Args [ArgsAt] (
+                        value: RequiredArg<AnyType> => Arg::positional("value"),
+                        tab_size: OptionalArg<ExactIntType<usize>> => Arg::long_flag("tab-size"),
+                        max_line_size: OptionalArg<ExactIntType<usize>> => Arg::long_flag("max-line-size")
+                    ) -> None,
+
+                    |at, Args { value, tab_size, max_line_size }, _, ctx| {
+                        let at = format!("dbg-type [{}]:", dbg_loc(at, ctx.files_map()).bright_yellow());
+
+                        println!("{} {}", at.bright_magenta(), value.get_type().render_colored(ctx, PrettyPrintOptions {
+                            pretty: true,
+                            line_prefix_size: at.chars().count(),
+                            max_line_size: max_line_size.or_else(|| terminal_size().map(|(Width(width), _)| usize::from(width))).unwrap_or(30),
+                            tab_size: tab_size.unwrap_or(4)
+                        }));
+
+                        Ok(None)
+                    }
+                ),
+            ),
         ],
 
         vars: vec![(GEN_PROMPT_VAR_NAME, true, RuntimeValue::Null)],
