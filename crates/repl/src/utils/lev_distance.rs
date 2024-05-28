9dc353db64a758b1adca27daf97a52713dcfe819
Clément Nerma <clement.nerma@gmail.com>
2024-05-28 16:47:37 +0200

    <lost commit message>

--- crates/repl/src/utils/lev_distance.rs
+++ crates/repl/src/utils/lev_distance.rs
@@ -1,36 +1,12 @@
-// This file is adapted from the rust compiler project:
-// https://github.com/rust-lang/rust/blob/cf9ed0dd5836201843d28bbad50abfbe1913af2a/compiler/rustc_span/src/lev_distance.rs#L1
-// https://github.com/rust-lang/rust/blob/cf9ed0dd5836201843d28bbad50abfbe1913af2a/LICENSE-MIT
-//
-// - the rust compiler-specific symbol::Symbol has been replaced by &str
-// - unstable feature .then_some has been replaced by an if ... else expression
-
-//! Levenshtein distances.
-//!
-//! The [Levenshtein distance] is a metric for measuring the difference between two strings.
-//!
-//! [Levenshtein distance]: https://en.wikipedia.org/wiki/Levenshtein_distance
-
 use std::cmp;
 
-/// Finds the Levenshtein distance between two strings.
+/// Compute the Levenshtein distance between two strings.
 pub fn levenshtein_distance(a: &str, b: &str) -> usize {
-    lev_distance(a, b, usize::max_value()).unwrap_or(usize::max_value())
-}
-
-/// Finds the Levenshtein distance between two strings.
-///
-/// Returns None if the distance exceeds the limit.
-fn lev_distance(a: &str, b: &str, limit: usize) -> Option<usize> {
     let n = a.chars().count();
     let m = b.chars().count();
-    let min_dist = if n < m { m - n } else { n - m };
 
-    if min_dist > limit {
-        return None;
-    }
     if n == 0 || m == 0 {
-        return Some(min_dist);
+        return usize::MAX;
     }
 
     let mut dcol: Vec<_> = (0..=m).collect();
@@ -51,38 +27,5 @@ fn lev_distance(a: &str, b: &str, limit: usize) -> Option<usize> {
         }
     }
 
-    if dcol[m] <= limit {
-        Some(dcol[m])
-    } else {
-        None
-    }
+    dcol[m]
 }
-
-// This file is copied from the rust compiler project:
-// https://github.com/rust-lang/rust/blob/cf9ed0dd5836201843d28bbad50abfbe1913af2a/compiler/rustc_span/src/lev_distance.rs#L1
-// https://github.com/rust-lang/rust/blob/cf9ed0dd5836201843d28bbad50abfbe1913af2a/LICENSE-MIT
-
-// Permission is hereby granted, free of charge, to any
-// person obtaining a copy of this software and associated
-// documentation files (the "Software"), to deal in the
-// Software without restriction, including without
-// limitation the rights to use, copy, modify, merge,
-// publish, distribute, sublicense, and/or sell copies of
-// the Software, and to permit persons to whom the Software
-// is furnished to do so, subject to the following
-// conditions:
-
-// The above copyright notice and this permission notice
-// shall be included in all copies or substantial portions
-// of the Software.
-
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
-// ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
-// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
-// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
-// SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
-// IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-// DEALINGS IN THE SOFTWARE.
-// Footer
