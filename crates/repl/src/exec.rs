bf9542b497dc007485fbac79785184b4ca643e7f
Clément Nerma <clement.nerma@gmail.com>
2024-05-28 16:47:40 +0200

    <lost commit message>

--- crates/repl/src/exec.rs
+++ crates/repl/src/exec.rs
@@ -1,38 +1,46 @@
-use parsy::{Eaten, FileId, Parser};
-use reshell_checker::output::CheckerOutput;
+use parsy::{FileId, Parser};
 use reshell_parser::{ast::Program, files::SourceFileLocation};
 use reshell_runtime::{context::Context, exec::run_program};
 
-use crate::reports::ReportableError;
-
-pub fn code_check_script(
-    input: &str,
-    loc: SourceFileLocation,
-    parser: &impl Parser<Program>,
-    ctx: &mut Context,
-) -> Result<(Eaten<Program>, CheckerOutput), ReportableError> {
-    let file_id = ctx.files_map().register_file(loc, input.to_string());
-
-    let parsed = parser
-        .parse_str_as_file(input, FileId::SourceFile(file_id))
-        .map_err(ReportableError::Parsing)?;
-
-    reshell_checker::check(
-        &parsed.data,
-        ctx.generate_checker_scopes(),
-        Some(ctx.generate_checker_output()),
-    )
-    .map(|checker_out| (parsed, checker_out))
-    .map_err(ReportableError::Checking)
-}
+use crate::{cmd::ExecArgs, reports::ReportableError};
 
 pub fn run_script(
     input: &str,
     file_loc: SourceFileLocation,
     parser: &impl Parser<Program>,
+    exec_args: ExecArgs,
     ctx: &mut Context,
 ) -> Result<(), ReportableError> {
-    let (parsed, checker_output) = code_check_script(input, file_loc, parser, ctx)?;
+    let ExecArgs {
+        print_ast,
+        print_checker_output,
+        only_check,
+    } = exec_args;
+
+    let file_id = ctx.files_map().register_file(file_loc, input.to_string());
+
+    let parsed = parser
+        .parse_str_as_file(input, FileId::SourceFile(file_id))
+        .map_err(ReportableError::Parsing)?;
+
+    if print_ast {
+        println!("AST: {parsed:#?}");
+    }
+
+    let checker_output = reshell_checker::check(
+        &parsed.data,
+        ctx.generate_checker_scopes(),
+        Some(ctx.generate_checker_output()),
+    )
+    .map_err(ReportableError::Checking)?;
+
+    if print_checker_output {
+        println!("Checker output: {checker_output:#?}");
+    }
+
+    if only_check {
+        return Ok(());
+    }
 
     run_program(&parsed, checker_output, ctx)
         .map_err(|err| ReportableError::Runtime(err, Some(parsed)))
