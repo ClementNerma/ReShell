ce3bc7fac0392f02953b1a2d77a36c1f6ad0406f
Clément Nerma <clement.nerma@gmail.com>
2024-05-28 16:47:39 +0200

    <lost commit message>

--- crates/repl/src/paths.rs
+++ crates/repl/src/paths.rs
@@ -5,13 +5,13 @@ use crate::utils::lazy_cell::LazyCell;
 pub static HOME_DIR: LazyCell<Option<PathBuf>> = LazyCell::new(dirs::home_dir);
 
 pub static SHELL_DATA_DIR: LazyCell<Option<PathBuf>> =
-    LazyCell::new(|| dirs::config_local_dir().map(|dir| dir.join("reshell")));
+    LazyCell::new(|| dirs::data_local_dir().map(|dir| dir.join("reshell")));
 
 pub static INIT_SCRIPT_PATH: LazyCell<Option<PathBuf>> =
     LazyCell::new(|| SHELL_DATA_DIR.as_ref().map(|dir| dir.join("init.rsh")));
 
-// pub static HISTORY_PATH: LazyCell<Option<PathBuf>> = LazyCell::new(|| {
-//     SHELL_DATA_DIR
-//         .as_ref()
-//         .map(|dir| dir.join("history.rsh_history"))
-// });
+pub static HISTORY_PATH: LazyCell<Option<PathBuf>> = LazyCell::new(|| {
+    SHELL_DATA_DIR
+        .as_ref()
+        .map(|dir| dir.join("history.rsh_history"))
+});
