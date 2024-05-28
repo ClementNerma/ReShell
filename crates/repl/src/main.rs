df1fb8ca190f7603098c9f70dacb3cb3607372a8
Clément Nerma <clement.nerma@gmail.com>
2024-05-28 16:47:52 +0200

    Remove Rust's 'lazy_cell' feature marker

--- crates/repl/src/main.rs
+++ crates/repl/src/main.rs
@@ -1,8 +1,6 @@
 #![forbid(unsafe_code)]
 #![forbid(unused_must_use)]
 #![warn(unused_crate_dependencies)]
-// TODO: nightly
-#![feature(lazy_cell)]
 #![feature(let_chains)]
 
 use std::{fs, path::PathBuf, process::ExitCode, time::Instant};
