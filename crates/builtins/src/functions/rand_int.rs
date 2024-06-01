5d5bc12827772c094258734da2653424be142ec6
Clément Nerma <clement.nerma@gmail.com>
2024-06-01 14:44:04 +0200

    Allow to specific ranges in randInt

--- crates/builtins/src/functions/rand_int.rs
+++ crates/builtins/src/functions/rand_int.rs
@@ -1,14 +1,35 @@
+use rand::Rng;
+
 crate::define_internal_fn!(
     "randInt",
 
-    ()
+    (
+        low_bound: OptionalArg<IntType> = Arg::positional("lowBound"),
+        high_bound: OptionalArg<IntType> = Arg::positional("highBound")
+    )
 
     -> Some(IntType::direct_underlying_type())
 );
 
 fn run() -> Runner {
-    Runner::new(|_, Args {}, _, _| {
-        let rand = rand::random::<i64>();
-        Ok(Some(RuntimeValue::Int(rand)))
-    })
+    Runner::new(
+        |_,
+         Args {
+             low_bound,
+             high_bound,
+         },
+         _,
+         _| {
+            let mut rand = rand::thread_rng();
+
+            let num = match (low_bound, high_bound) {
+                (Some(low_bound), Some(high_bound)) => rand.gen_range(low_bound..=high_bound),
+                (Some(low_bound), None) => rand.gen_range(low_bound..=i64::MAX),
+                (None, Some(high_bound)) => rand.gen_range(i64::MIN..=high_bound),
+                (None, None) => rand.gen(),
+            };
+
+            Ok(Some(RuntimeValue::Int(num)))
+        },
+    )
 }
