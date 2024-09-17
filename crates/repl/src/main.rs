8795debda4c2104aa47f82f5e12b8170bcbdf80f
Clément Nerma <clement.nerma@gmail.com>
2024-09-17 13:57:17 +0200

    Remove unneeded feature marker

--- crates/repl/src/main.rs
+++ crates/repl/src/main.rs
@@ -1,7 +1,6 @@
 #![forbid(unsafe_code)]
 #![forbid(unused_must_use)]
 #![warn(unused_crate_dependencies)]
-#![feature(let_chains)]
 
 use std::{fs, path::PathBuf, process::ExitCode, time::Instant};
 
