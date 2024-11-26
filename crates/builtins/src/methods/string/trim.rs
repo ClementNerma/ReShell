c52bfb83623757481f61c31c3206137d6807eff9
Clément Nerma <clement.nerma@gmail.com>
2024-11-26 13:29:35 +0100

    Make .trim() accept an optional pattern

--- crates/builtins/src/methods/string/trim.rs
+++ crates/builtins/src/methods/string/trim.rs
@@ -4,12 +4,30 @@ define_internal_fn!(
     "trim",
 
     (
-        string: RequiredArg<StringType> = Arg::method_self()
+        string: RequiredArg<StringType> = Arg::method_self(),
+        pattern: OptionalArg<StringType> = Arg::positional("pattern")
     )
 
     -> Some(StringType::value_type())
 );
 
 fn run() -> Runner {
-    Runner::new(|_, Args { string }, _, _| Ok(Some(RuntimeValue::String(string.trim().to_owned()))))
+    Runner::new(|_, Args { string, pattern }, args_at, ctx| {
+        let trimmed = match pattern {
+            Some(pattern) => {
+                if pattern.len() != 1 {
+                    return Err(ctx.throw(
+                        args_at.pattern.unwrap(),
+                        format!("expected a single-character pattern, got: '{pattern:?}'"),
+                    ));
+                }
+
+                string.trim_matches(pattern.chars().next().unwrap())
+            }
+
+            None => string.trim(),
+        };
+
+        Ok(Some(RuntimeValue::String(trimmed.to_owned())))
+    })
 }
