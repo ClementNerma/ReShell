548a2015242edbf583b27ed4287a7b7d1bcd0fdd
Clément Nerma <clement.nerma@gmail.com>
2024-12-09 20:19:05 +0100

    Improve .toStruct() and .toMap() methods

--- crates/builtins/src/methods/map/to_struct.rs
+++ crates/builtins/src/methods/map/to_struct.rs
@@ -1,13 +1,26 @@
+use reshell_runtime::gc::GcCell;
+
 crate::define_internal_fn!(
+    //
+    // convert a struct to a map
+    //
+
     "toStruct",
 
     (
-        map: RequiredArg<UntypedMapType> = RequiredArg::method_self()
+        obj: RequiredArg<UntypedMapType> = Arg::method_self(),
+        linked: PresenceFlag = Arg::long_flag("linked")
     )
 
     -> Some(UntypedStructType::value_type())
 );
 
 fn run() -> Runner {
-    Runner::new(|_, Args { map }, _, _| Ok(Some(RuntimeValue::Struct(map))))
+    Runner::new(|_, Args { obj, linked }, _, _| {
+        Ok(Some(RuntimeValue::Struct(if linked {
+            obj
+        } else {
+            GcCell::new(obj.read_promise_no_write().clone())
+        })))
+    })
 }
--- crates/builtins/src/methods/structure/to_map.rs
+++ crates/builtins/src/methods/structure/to_map.rs
@@ -5,19 +5,22 @@ crate::define_internal_fn!(
     // convert a struct to a map
     //
 
-    "structToMap",
+    "toMap",
 
     (
-        obj: RequiredArg<UntypedStructType> = Arg::method_self()
+        obj: RequiredArg<UntypedStructType> = Arg::method_self(),
+        linked: PresenceFlag = Arg::long_flag("linked")
     )
 
     -> Some(UntypedMapType::value_type())
 );
 
 fn run() -> Runner {
-    Runner::new(|_, Args { obj }, _, _| {
-        let entries = obj.read_promise_no_write().clone();
-
-        Ok(Some(RuntimeValue::Map(GcCell::new(entries))))
+    Runner::new(|_, Args { obj, linked }, _, _| {
+        Ok(Some(RuntimeValue::Map(if linked {
+            obj
+        } else {
+            GcCell::new(obj.read_promise_no_write().clone())
+        })))
     })
 }
