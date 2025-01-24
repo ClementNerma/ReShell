1f281192922c6a125963d5d0cc2acfc43ed7a9df
Clément Nerma <clement.nerma@gmail.com>
2025-01-24 13:31:30 +0100

    Fix: external binaries handling in 'which'

--- crates/builtins/src/functions/which.rs
+++ crates/builtins/src/functions/which.rs
@@ -18,36 +18,42 @@ define_internal_fn!(
 
 fn run() -> Runner {
     Runner::new(|_, Args { command, external }, _, ctx| {
-        if external {
-            match ctx.binaries_resolver().resolve_binary_path(&command) {
-                Ok(path) => {
-                    println!("External binary at: {}", path.to_string_lossy().underline());
-                }
+        if !external {
+            for scope in ctx.visible_scopes_content() {
+                let ScopeContent {
+                    vars: _,
+                    fns,
+                    methods,
+                    cmd_aliases,
+                } = scope;
 
-                Err(err) => {
-                    eprintln!("{}", err.bright_red());
-                }
-            }
+                if let Some(methods) = command
+                    .strip_prefix('.')
+                    .and_then(|method_name| methods.get(method_name))
+                {
+                    for method in methods {
+                        println!(
+                            "* Method declared at: {}\n  {}\n",
+                            match method.name_at {
+                                RuntimeCodeRange::Parsed(at) => {
+                                    at.display(ctx.files_map(), PrettyPrintOptions::inline())
+                                        .to_string()
+                                        .underline()
+                                }
 
-            return Ok(None);
-        }
+                                RuntimeCodeRange::Internal(str) => {
+                                    format!("internal location: {str}").italic()
+                                }
+                            },
+                            method.value.display(ctx, PrettyPrintOptions::inline())
+                        );
+                    }
 
-        for scope in ctx.visible_scopes_content() {
-            let ScopeContent {
-                vars: _,
-                fns,
-                methods,
-                cmd_aliases,
-            } = scope;
-
-            if let Some(methods) = command
-                .strip_prefix('.')
-                .and_then(|method_name| methods.get(method_name))
-            {
-                for method in methods {
+                    break;
+                } else if let Some(func) = fns.get(&command) {
                     println!(
-                        "* Method declared at: {}\n  {}\n",
-                        match method.name_at {
+                        "Function declared at: {}\n\n{}",
+                        match func.name_at {
                             RuntimeCodeRange::Parsed(at) => {
                                 at.display(ctx.files_map(), PrettyPrintOptions::inline())
                                     .to_string()
@@ -58,51 +64,43 @@ fn run() -> Runner {
                                 format!("internal location: {str}").italic()
                             }
                         },
-                        method.value.display(ctx, PrettyPrintOptions::inline())
+                        func.value.display(ctx, PrettyPrintOptions::inline())
                     );
+
+                    break;
+                } else if let Some(cmd_alias) = cmd_aliases.get(&command) {
+                    let at = cmd_alias.value.content.at;
+
+                    println!(
+                        "Alias declared at: {}\n\n{}",
+                        cmd_alias
+                            .value
+                            .name_declared_at
+                            .display(ctx.files_map(), PrettyPrintOptions::inline())
+                            .to_string()
+                            .underline(),
+                        match at.start.file_id {
+                            FileId::SourceFile(id) => {
+                                let source = ctx.files_map().get_file(id).unwrap().content;
+                                source[at.start.offset..at.start.offset + at.len].italic()
+                            }
+
+                            _ => "<no source available>".italic(),
+                        }
+                    );
+
+                    break;
                 }
+            }
+        }
 
-                break;
-            } else if let Some(func) = fns.get(&command) {
-                println!(
-                    "Function declared at: {}\n\n{}",
-                    match func.name_at {
-                        RuntimeCodeRange::Parsed(at) => {
-                            at.display(ctx.files_map(), PrettyPrintOptions::inline())
-                                .to_string()
-                                .underline()
-                        }
+        match ctx.binaries_resolver().resolve_binary_path(&command) {
+            Ok(path) => {
+                println!("External binary at: {}", path.to_string_lossy().underline());
+            }
 
-                        RuntimeCodeRange::Internal(str) => {
-                            format!("internal location: {str}").italic()
-                        }
-                    },
-                    func.value.display(ctx, PrettyPrintOptions::inline())
-                );
-
-                break;
-            } else if let Some(cmd_alias) = cmd_aliases.get(&command) {
-                let at = cmd_alias.value.content.at;
-
-                println!(
-                    "Alias declared at: {}\n\n{}",
-                    cmd_alias
-                        .value
-                        .name_declared_at
-                        .display(ctx.files_map(), PrettyPrintOptions::inline())
-                        .to_string()
-                        .underline(),
-                    match at.start.file_id {
-                        FileId::SourceFile(id) => {
-                            let source = ctx.files_map().get_file(id).unwrap().content;
-                            source[at.start.offset..at.start.offset + at.len].italic()
-                        }
-
-                        _ => "<no source available>".italic(),
-                    }
-                );
-
-                break;
+            Err(err) => {
+                eprintln!("{}", err.bright_red());
             }
         }
 
