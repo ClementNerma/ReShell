dd847c9be67349631459286fdc02d7a383e93770
Clément Nerma <clement.nerma@gmail.com>
2025-05-01 13:38:27 +0200

    Add docs to public functions of syntax-highlighter

--- crates/syntax-highlighter/src/lib.rs
+++ crates/syntax-highlighter/src/lib.rs
@@ -21,6 +21,16 @@ pub enum CheckCmdType {
     BroadCmd, // External cmd or function name or alias
 }
 
+/// Perform syntax highlighting on a given input
+///
+/// This function returns a list of syntax items along with their positions in the input.
+///
+/// The `cmd_checker` argument is a function that determines if a given command (with the provided type)
+/// exists or not. This function should run very fast as it will be called for each single command, method
+/// and function call in the program.
+///
+/// Note that the first call to this function make take longer due to the lazy-initialized parser warming up.
+/// If you prefer warming up before calling this function, use [`preinit_lazy_syntax_highlighter`].
 pub fn syntax_highlight(
     input: &str,
     cmd_checker: impl FnMut(&str, CheckCmdType) -> bool + Send + Sync + 'static,
@@ -30,4 +40,14 @@ pub fn syntax_highlight(
         &self::highlighter::RULE_SET,
         &Arc::new(Mutex::new(Box::new(cmd_checker))),
     )
+}
+
+/// The syntax highlighter engine is created lazily from a lot of different regular expressions.
+///
+/// This function triggers the initialization of the engine and ensures calling [`syntax_highlight`] for
+/// the first time will not take longer than required.
+///
+/// Note that this function may take several dozens of milliseconds to complete.
+pub fn preinit_lazy_syntax_highlighter() {
+    let _ = &self::highlighter::RULE_SET;
 }
