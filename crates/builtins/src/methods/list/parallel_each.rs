cad2cd81055f5e9596b099a3375ecdbedbe44485
Clément Nerma <clement.nerma@gmail.com>
2026-01-07 21:52:16 +0100

    feat: allow specifying max number of threads for .parallelEach()

--- crates/builtins/src/methods/list/parallel_each.rs
+++ crates/builtins/src/methods/list/parallel_each.rs
@@ -1,6 +1,6 @@
 use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 
-use crate::{declare_typed_fn_handler, utils::call_fn_checked};
+use crate::{declare_typed_fn_handler, helpers::runner::run_parallel, utils::call_fn_checked};
 
 crate::define_internal_fn!(
     //
@@ -11,7 +11,8 @@ crate::define_internal_fn!(
 
     (
         list: RequiredArg<UntypedListType> = Arg::method_self(),
-        for_each_fn: RequiredArg<ForEachFn> = Arg::positional("for_each_fn")
+        for_each_fn: RequiredArg<ForEachFn> = Arg::positional("for_each_fn"),
+        max_threads: OptionalArg<ExactIntType<usize>> = Arg::long_and_short_flag("max_threads", 't')
     )
 
     -> VoidType
@@ -20,24 +21,38 @@ crate::define_internal_fn!(
 declare_typed_fn_handler!(ForEachFn(value: AnyType) -> AnyType);
 
 fn run() -> Runner {
-    Runner::new(|_, Args { list, for_each_fn }, args_at, ctx| {
-        let for_each_fn =
-            LocatedValue::new(args_at.for_each_fn, RuntimeValue::Function(for_each_fn));
+    Runner::new(
+        |_,
+         Args {
+             list,
+             for_each_fn,
+             max_threads,
+         },
+         args_at,
+         ctx| {
+            let for_each_fn =
+                LocatedValue::new(args_at.for_each_fn, RuntimeValue::Function(for_each_fn));
 
-        list.read(args_at.list)
-            .par_iter()
-            .map(|value| {
-                let mut ctx = ctx.clone();
+            run_parallel(
+                || {
+                    list.read(args_at.list)
+                        .par_iter()
+                        .map(|value| {
+                            let mut ctx = ctx.clone();
 
-                call_fn_checked(
-                    &for_each_fn,
-                    &ForEachFn::signature(),
-                    vec![value.clone()],
-                    &mut ctx,
-                )
-            })
-            .collect::<Result<Vec<_>, _>>()?;
+                            call_fn_checked(
+                                &for_each_fn,
+                                &ForEachFn::signature(),
+                                vec![value.clone()],
+                                &mut ctx,
+                            )
+                        })
+                        .collect::<Result<Vec<_>, _>>()
+                },
+                max_threads,
+            )?;
 
-        Ok(None)
-    })
+            Ok(None)
+        },
+    )
 }
