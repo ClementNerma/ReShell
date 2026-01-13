ee7073c05eb488346c89e0a3219a965043ce5bb6
Clément Nerma <clement.nerma@gmail.com>
2026-01-13 23:34:06 +0100

    feat: allow creating a datetime() from a number of secs since EPOCH

--- crates/builtins/src/functions/datetime.rs
+++ crates/builtins/src/functions/datetime.rs
@@ -1,4 +1,4 @@
-use jiff::Zoned;
+use jiff::{Timestamp, Zoned, tz::TimeZone};
 use reshell_runtime::gc::GcReadOnlyCell;
 
 use crate::{define_internal_fn, types::DateTimeValue};
@@ -6,15 +6,32 @@ use crate::{define_internal_fn, types::DateTimeValue};
 define_internal_fn!(
     "datetime",
 
-    ()
+    (
+        secs_since_epoch: OptionalArg<ExactIntType<i64>> = Arg::positional("from-secs-since-unix-epoch")
+    )
 
     -> CustomType<DateTimeValue>
 );
 
 fn run() -> Runner {
-    Runner::new(|_, Args {}, _, _| {
+    Runner::new(|_, Args { secs_since_epoch }, args_at, ctx| {
+        let moment = match secs_since_epoch {
+            None => Zoned::now(),
+
+            Some(secs) => {
+                let ts = Timestamp::new(secs, 0).map_err(|err| {
+                    ctx.error(
+                        args_at.secs_since_epoch.unwrap(),
+                        format!("Failed to create date time from provided seconds: {err}"),
+                    )
+                })?;
+
+                Zoned::new(ts, TimeZone::system())
+            }
+        };
+
         Ok(Some(RuntimeValue::Custom(GcReadOnlyCell::new(Box::new(
-            DateTimeValue(Zoned::now()),
+            DateTimeValue(moment),
         )))))
     })
 }
