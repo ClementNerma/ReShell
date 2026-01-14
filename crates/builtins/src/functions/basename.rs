bec7a54e00c33675ab5f291cba77022c54ac9b3d
Clément Nerma <clement.nerma@gmail.com>
2026-01-14 19:47:47 +0100

    feat(std): use Rust's Path::file_name() for basename()

--- crates/builtins/src/functions/basename.rs
+++ crates/builtins/src/functions/basename.rs
@@ -14,11 +14,12 @@ define_internal_fn!(
 
 fn run() -> Runner {
     Runner::new(|_, Args { path }, _, _| {
-        Ok(Some(match Path::new(&path).components().next_back() {
-            Some(component) => {
-                RuntimeValue::String(component.as_os_str().to_str().unwrap().to_owned())
-            }
-            None => RuntimeValue::Null,
-        }))
+        Ok(Some(
+            Path::new(&path)
+                .file_name()
+                .map_or(RuntimeValue::Null, |str| {
+                    RuntimeValue::String(str.to_str().unwrap().to_owned())
+                }),
+        ))
     })
 }
