7e3017e181a7758622efb1d5cc363ab3de2f4c32
Clément Nerma <clement.nerma@gmail.com>
2026-03-30 22:51:57 +0200

    feat(std): improve typing of 'echo' function

--- crates/builtins/src/functions/echo.rs
+++ crates/builtins/src/functions/echo.rs
@@ -1,6 +1,9 @@
 use colored::{Color, Colorize};
 
-use crate::methods::{StringifyableType, stringify_value};
+use crate::{
+    declare_string_literal_type,
+    methods::{StringifyableType, stringify_value},
+};
 
 crate::define_internal_fn!(
     //
@@ -11,11 +14,32 @@ crate::define_internal_fn!(
 
     (
         message: RequiredArg<StringifyableType> = Arg::positional("message"),
-        color: OptionalArg<StringType> = Arg::long_and_short_flag("color", 'c'),
+        color: OptionalArg<ColorNameType> = Arg::long_and_short_flag("color", 'c'),
         no_newline: PresenceFlag = Arg::long_and_short_flag("no-newline", 'n')
     )
 
     -> VoidType
+);
+
+declare_string_literal_type!(
+    ColorNameType => enum ColorName {
+        Black ("black"),
+        Red ("red"),
+        Green ("green"),
+        Yellow ("yellow"),
+        Blue ("blue"),
+        Magenta ("magenta"),
+        Cyan ("cyan"),
+        White ("white"),
+        BrightBlack ("brightBlack"),
+        BrightRed ("brightRed"),
+        BrightGreen ("brightGreen"),
+        BrightYellow ("brightYellow"),
+        BrightBlue ("brightBlue"),
+        BrightMagenta ("brightMagenta"),
+        BrightCyan ("brightCyan"),
+        BrightWhite ("brightWhite")
+    }
 );
 
 fn run() -> Runner {
@@ -26,37 +50,32 @@ fn run() -> Runner {
              color,
              no_newline,
          },
-         args_at,
-         ctx| {
+         _,
+         _| {
             let message = stringify_value(message);
 
             match color {
                 None => println!("{message}"),
 
                 Some(color) => {
-                    let color = match color.as_str() {
-                        "black" => Color::Black,
-                        "red" => Color::Red,
-                        "green" => Color::Green,
-                        "yellow" => Color::Yellow,
-                        "blue" => Color::Blue,
-                        "magenta" => Color::Magenta,
-                        "cyan" => Color::Cyan,
-                        "white" => Color::White,
-                        "brightBlack" => Color::BrightBlack,
-                        "brightRed" => Color::BrightRed,
-                        "brightGreen" => Color::BrightGreen,
-                        "brightYellow" => Color::BrightYellow,
-                        "brightBlue" => Color::BrightBlue,
-                        "brightMagenta" => Color::BrightMagenta,
-                        "brightCyan" => Color::BrightCyan,
-                        "brightWhite" => Color::BrightWhite,
-                        _ => {
-                            return Err(ctx.throw(
-                                args_at.color.unwrap(),
-                                format!("unknown color '{color}'"),
-                            ));
-                        }
+                    let color = match color {
+                        ColorName::Black => Color::Black,
+                        ColorName::Red => Color::Red,
+                        ColorName::Green => Color::Green,
+                        ColorName::Yellow => Color::Yellow,
+                        ColorName::Blue => Color::Blue,
+                        ColorName::Magenta => Color::Magenta,
+                        ColorName::Cyan => Color::Cyan,
+                        ColorName::White => Color::White,
+
+                        ColorName::BrightBlack => Color::BrightBlack,
+                        ColorName::BrightRed => Color::BrightRed,
+                        ColorName::BrightGreen => Color::BrightGreen,
+                        ColorName::BrightYellow => Color::BrightYellow,
+                        ColorName::BrightBlue => Color::BrightBlue,
+                        ColorName::BrightMagenta => Color::BrightMagenta,
+                        ColorName::BrightCyan => Color::BrightCyan,
+                        ColorName::BrightWhite => Color::BrightWhite,
                     };
 
                     let message = message.color(color);
